0af945d8f914b76548b850ae09409d5561ed90ee
kpspavan <kpspavansrinivas@gmail.com>
2024-07-10 23:07:37 +0530

    added contact form and integrated mail

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,56 +1,107 @@
-// ContactForm.js
 import React, { useState } from "react";
-import { Modal, Form, Input, Button } from "antd";
+import { Modal, Form, Input, Button, message } from "antd";
+import { CSSTransition } from "react-transition-group";
+import "animate.css";
+import emailjs from "emailjs-com";
+import "./Contact.css";
 
 const ContactForm = ({ visible, onClose }) => {
   const [form] = Form.useForm();
+  const [animate, setAnimate] = useState(false);
 
   const handleOk = () => {
     form
       .validateFields()
       .then((values) => {
-        console.log("Form Values:", values);
+        sendEmail(values);
         onClose();
       })
       .catch((info) => {
         console.log("Validate Failed:", info);
+        setAnimate(true);
+        setTimeout(() => setAnimate(false), 1500);
       });
   };
 
+  const sendEmail = (formValues) => {
+    const { name, phone } = formValues;
+    const templateParams = {
+      name,
+      phone,
+    };
+
+    emailjs
+      .send(
+        "service_5ig89ar",
+        "template_3xajdmt",
+        templateParams,
+        "tD1S0ld3xVyrSU6iF"
+      )
+      .then(
+        (result) => {
+          console.log("Email successfully sent!", result.text);
+          message.success("Email sent successfully!");
+          form.resetFields();
+        },
+        (error) => {
+          console.log("Failed to send email.", error.text);
+          message.error("Failed to send email.");
+        }
+      );
+  };
+
   return (
-    <Modal
-      title="Contact Form"
-      visible={visible}
-      onOk={handleOk}
-      onCancel={onClose}
-      footer={[
-        <Button key="back" onClick={onClose}>
-          Cancel
-        </Button>,
-        <Button key="submit" type="primary" onClick={handleOk}>
-          Submit
-        </Button>,
-      ]}
-    >
-      <Form form={form} layout="vertical" name="contact_form">
-        <Form.Item
-          name="name"
-          label="Name"
-          rules={[{ required: true, message: "Please input your name!" }]}
+    <CSSTransition in={visible} timeout={300} classNames="modal" unmountOnExit>
+      <Modal
+        title="Contact Form"
+        visible={visible}
+        onOk={handleOk}
+        onCancel={onClose}
+        footer={[
+          <Button key="back" onClick={onClose}>
+            Cancel
+          </Button>,
+          <Button key="submit" type="primary" onClick={handleOk}>
+            Submit
+          </Button>,
+        ]}
+      >
+        <Form
+          form={form}
+          layout="vertical"
+          name="contact_form"
+          className={
+            animate
+              ? "animate__animated animate__bounceIn animate__fadeOut"
+              : ""
+          }
         >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="phone"
-          label="Phone Number"
-          rules={[
-            { required: true, message: "Please input your phone number!" },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-      </Form>
-    </Modal>
+          <Form.Item
+            name="name"
+            label="Enter your Name"
+            rules={[
+              { required: true, message: "Please input your name!" },
+              { min: 3, message: "Name must be at least 3 characters!" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="phone"
+            label="Enter your Phone Number"
+            rules={[
+              { required: true, message: "Please input your phone number!" },
+              {
+                pattern: /^[0-9]{10,}$/,
+                message: "Phone number must be at least 10 digits!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </CSSTransition>
   );
 };
 
